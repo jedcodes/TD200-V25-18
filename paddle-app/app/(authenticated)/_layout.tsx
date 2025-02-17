a1e0280f90a136dc0bae86563a36ea460dde01b8
Jack Delamou <jackdelamou.97@gmail.com>
2025-02-17 13:11:09 +0100

    initial commit

---
+++ paddle-app/app/(authenticated)/_layout.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function AuthenticatedRoutesLayout() {
+  return (
+    <View>
+      <Text>AuthenticatedRoutesLayout</Text>
+    </View>
+  );
+}
---
+++ paddle-app/app/(public)/_layout.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function PublicRoutesLayout() {
+  return (
+    <View>
+      <Text>PublicRoutesLayout</Text>
+    </View>
+  );
+}
